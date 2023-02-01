6b1177e775bc281c5e9865d1e5aadf9eb4620d47
Kongyveho <124108903+Kongyveho@users.noreply.github.com>
2023-02-01 10:13:16 +0800

    Update Bz.js

--- Bz.js
+++ Bz.js
@@ -6,13 +6,22 @@
 
 *******************************
 [rewrite_local]
-^http[s]?:\/\/bzpp2.iwzbz.com\/api\/v1.1\/user\/  url script-response-body https://github.com/Kongyveho/tux/blob/main/Bz.js
+^http[s]?:\/\/bzpp2.iwzbz.com\/api\/v1.1\/user\/getpwnewios_v.+$ url script-response-body iwzbz.js
 [mitm] 
-hostname = bzpp2.iwzbz.com
+hostname = *.iwzbz.*
+*******************************
+Surge
+
+[Script]
+^http[s]?:\/\/bzpp2.iwzbz.com\/api\/v1.1\/user\/getpwnewios_v.+$ requires-body=1,max-size=0,script-path=iwzbz.js
+
+[MITM]
+hostname = *.iwzbz.*
 
 *******************************/
-var body=$response['body'];
-obj.vipLevel=  3;
+var obj = JSON.parse($response.body);
+    obj.vipLevel=  3;
 obj.expires=  "2025;
 obj.vipTipsType=  0;
-   $done({'body':body});
+    $done({body: JSON.stringify(obj)});
+
