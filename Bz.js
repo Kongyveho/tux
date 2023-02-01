c294d7c7958e42f2fd495d6f938f88e64ec8e76d
Kongyveho <124108903+Kongyveho@users.noreply.github.com>
2023-02-01 17:23:43 +0800

    Update Bz.js

--- Bz.js
+++ Bz.js
@@ -19,9 +19,8 @@ Surge
 hostname = *.iwzbz.*
 
 *******************************/
-var obj = JSON.parse($response.body);
-    obj.vipLevel=  3;
-    obj.expires=  "2025;
-    obj.vipTipsType=  0;
-    $done({body: JSON.stringify(obj)});
-
+var body=$response.body;
+body = body.replace(/vipLevel\": \d/g,'vipLevel": 3');
+body = body.replace(/expires\": "1970/g,'expires": "2025');
+body = body.replace(/vipTipsType\": -1/g,'vipTipsType": 0');
+$done(body);
